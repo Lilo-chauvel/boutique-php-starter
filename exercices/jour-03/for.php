a52a1c4193390b50a1cd9c3471aaf13de514ff0a
Lilo-chauvel <lilo.chauvel@exemple.com>
2026-01-07 12:30:14 +0100

    J3 Tous les exos jusqu'au 6

---
+++ exercices/jour-03/for.php
@@ -0,0 +1,23 @@
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . "<br/>";
+}
+
+echo "<br/>";
+for ($i = 2; $i <= 20; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . "<br/>";
+    }
+
+}
+
+echo "<br/>";
+for ($i = 10; $i >= 0; $i--) {
+    echo $i . "<br/>";
+}
+
+echo "<br/>";
+for ($i = 1; $i <= 10; $i++) {
+    echo ($i * 7) . "<br/>";
+}
+?>
---
+++ exercices/jour-03/generation.php
@@ -0,0 +1,14 @@
+<?php
+$products = [];
+
+for($i=1;$i<=10;$i++){
+    $products["Produit" . $i] = 
+     [
+        "name" => "Produit" . $i,
+        "price" => rand(10,100),
+        "stock" => rand(0,50)
+    ];
+}
+
+print_r($products);
+?>
