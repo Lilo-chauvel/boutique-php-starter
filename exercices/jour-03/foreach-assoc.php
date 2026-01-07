56bdb2dad8c0375ece0535276ed9f7fd8fb2e598
Lilo-chauvel <lilo.chauvel@exemple.com>
2026-01-07 10:46:06 +0100

    J3 Exercice 2

---
+++ exercices/jour-03/foreach-assoc.php
@@ -0,0 +1,27 @@
+<?php
+$infoPersonnelle = [
+    "name" => "mark",
+    "age" => "44",
+    "city" => "Paris",
+    "job" => "Ingenieur"
+]
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <?php foreach ($infoPersonnelle as $key=>$info): ?>
+            <p><strong><?= $key?></strong> : <?=	$info?></p>
+        <?php endforeach ?>
+    </section>
+</body>
+
+</html>
